f67f9705be1f300bf63b1fda9f474181e90fb24d
Marie Lindström <zeldastardust@hotmail.com>
2020-10-22 23:38:51 +0200

    admin utkast

--- admin.php
+++ admin.php
@@ -1,44 +1,59 @@
 <?php
-include_once 'includes/header.php'; ?>
+require_once 'includes/header.php'; ?>
 
 <div id="container">
-    <div id="readExp">
+    <div id="exp">
+        <h1>Erfarenheter</h1>
+        <table class='table table-bordered table-hover'>
+ 
+            <!-- creating our table heading -->
+    <tr>
+        <th class='w-10-pct'>Plats</th>
+        <th class='w-10-pct'>Namn</th>
+        <th class='w-10-pct'>Kategori</th>
+        <th class='w-10-pct'>Start</th>
+        <th class='w-10-pct'>Stop</th>
+        <th class='w-10-pct'></th>
+    </tr>
+        </table>
+        <div id="experiences">
+        </div>
     </div>
-    <div id="create form">
-<!-- HTML form for creating a experience -->
-<h1 id="form h1">Lägg till erfarenhet</h1>
-<form action="" method="post">
+    <div id="expform">
 
-    <table class='table table-hover table-responsive table-bordered'>
+        <h1 id="form h1">Lägg till erfarenhet</h1>
+        <form action="" method="post">
 
-        <tr>
-            <td>Arbetsplats/lärosäte/Url</td>
-            <td><input type='text' name='place' class='form-control' /></td>
-        </tr>
+            <table class='table table-hover table-responsive table-bordered'>
 
-        <tr>
-            <td>Namn</td>
-            <td><input type='text' name='name' class='form-control' /></td>
-        </tr>
-        <tr>
-            <td>kategori</td>
-            <td><input type='text' name='category' class='form-control' /></td>
-        </tr>
-        <tr>
-            <td>Start</td>
-            <td><input type='date' name='startedu' class='form-control' /></td>
-        </tr>
-        <tr>
-            <td>Stop</td>
-            <td><input type='date' name='stopedu' class='form-control' /></td>
-        </tr>
-        <tr>
-            
-            <td>
-                <button type="submit" class="btn btn-primary">Create</button>
-            </td>
-        </tr>
-</form>
-</div>
-</div>
-<?php include("includes/footer.php");?>
+                <tr>
+                    <td>Arbetsplats/lärosäte/Url</td>
+                    <td><input type='text' name='place' class='form-control' /></td>
+                </tr>
+
+                <tr>
+                    <td>Namn</td>
+                    <td><input type='text' name='name' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>kategori</td>
+                    <td><input type='text' name='category' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Start</td>
+                    <td><input type='date' name='startedu' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Stop</td>
+                    <td><input type='date' name='stopedu' class='form-control' /></td>
+                </tr>
+                <tr>
+
+                    <td>
+                        <button type="submit" id="addExp" class="btn btn-primary">Create</button>
+                    </td>
+                </tr>
+        </form>
+    </div>
+
+        <?php include("includes/footer.php"); ?>
--- main.js
+++ main.js
@@ -1,20 +1,76 @@
-"use strict"
+"use strict";
 
 //variabler
 let experiencesEl = document.getElementById("experiences");
+let addExpbtn = document.getElementById("addExp");
+let placeInput= document.getElementById("place");
+let nameInput= document.getElementById("name");
+let categoryInput= document.getElementById("category");
+let starteduInput= document.getElementById("startedu");
+let stopeduInput= document.getElementById("stopedu");
+
 
 //eventlistener
 window.addEventListener("load", getExperiences);
+addExpbtn.addEventListener("click", addExp);
 
 //functions
 function getExperiences(){
     experiencesEl.innerHTML='';
-
+    
     fetch('http://localhost/projekt%20WUIII/apiProjekt/api/experiences.php')
     .then(response => response.json())
-    .then(data =>{
-        data.forEach(experiences=>{
-            console.log(experiences)
+    .then(data => {
+        data.records.forEach(experiences => {
+            experiencesEl.innerHTML+=
+            `<div class= "experiences"><tr>
+        Plats:${experiences.place}
+        Namn:${experiences.name}
+        Kategori:${experiences.category}
+        Start:${experiences.startedu}
+        Stop:${experiences.stopedu}
+        <button class='btn btn-danger delete-product-button'id="${experiences.id}" onClick="deleteExperience(${experiences.id})">Radera</button>
+        <button class='btn btn-info m-r-10px update-product-button' id="${experiences.id}" onClick="updateExperience(${experiences.id})">Uppdatera</button>
+        </tr></div>´;
+        </table>`;
+ 
+       
         })
     })
+}
+
+function deleteExperience(id){
+    fetch(`http://localhost/projekt%20WUIII/apiProjekt/api/experiences.php?id=`+ id,{
+        method:'DELETE',
+    })
+    .then(response =>response.json())
+    .then(data => {
+        getExperiences();
+    })
+    .catch(error=>{
+        console.log("Error:", error);
+    })
+}
+
+function addExp(){
+    let place = placeInput.value;
+    let name = nameInput.value;
+    let category= categoryInput.value;
+    let startedu= starteduInput.value;
+    let stopedu= stopeduInput.value;
+
+    let exp = {'place':place, 'name':name, 'category':category, 'startedu':startedu, 'stopedu':stopedu};
+
+    fetch('http://localhost/projekt%20WUIII/apiProjekt/api/experiences.php',{
+        method:"POST",
+        body: JSON.stringify(exp),
+    })
+    .then(response =>response.json())
+    .then (data =>{
+        getExperiences();
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    })
+
 }
